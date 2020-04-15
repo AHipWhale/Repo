d3b5bc2bf6cacfe536d434b3c801b356acf4742b
AHipWhale <charlie.choffat@gmail.com>
2020-04-15 15:41:26 +0200

    test

---
+++ AP/src/Pack/Main.java
@@ -0,0 +1,10 @@
+package Pack;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Vogel ding = new Vogel("Duif");
+        System.out.println("soort: " + ding.Soort());
+    }
+
+}
---
+++ AP/src/Pack/Vogel.java
@@ -0,0 +1,11 @@
+package Pack;
+
+public class Vogel {
+    private String soort;
+
+    public Vogel(String srt) {
+        soort = srt;
+    }
+
+    public String Soort() {return soort;}
+}

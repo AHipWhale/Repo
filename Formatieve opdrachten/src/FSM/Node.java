891fbe8923b10c6454a64cac7d4386b5a1691747
AHipWhale <charlie.choffat@gmail.com>
2020-05-06 15:40:27 +0200

    2a. machine met tekstinvoer

---
+++ Formatieve opdrachten/src/FSM/Node.java
@@ -0,0 +1,41 @@
+package FSM;
+
+public class Node {
+    private String naam;
+    private Node overgangA;
+    private Node overgangB;
+
+    public Node(String nm) {
+        naam = nm;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setOvergangA(Node overgangA) {
+        this.overgangA = overgangA;
+    }
+
+    public void setOvergangB(Node overgangB) {
+        this.overgangB = overgangB;
+    }
+
+    public Node getOvergangA() {
+        return overgangA;
+    }
+
+    public Node getOvergangB() {
+        return overgangB;
+    }
+
+    public Node verwerkInput(String s) {
+        if (s.equals("A")) {
+            return overgangA;
+        }
+        else {
+            return overgangB;
+        }
+    }
+}
+
